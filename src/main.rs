cf4ca985c9cee1cf01e987819db687662bb17392
numberss <red.ace.l@outlook.com>
2025-08-27 00:18:59 +0100

    works now :)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 // this is a program to convert a string to title case
 // e.g. "hello world" -> "Hello World"
-// i feel like this is quite bloated
-// maybe use structs or options?
 
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::path::Path;
 
 
 fn to_title_case(string: &str) -> String {
@@ -45,7 +44,7 @@ fn capitalise_word(word: &str) -> String {
 }
 
 fn get_exceptions() -> Vec<String> {
-    let file = File::open("exceptions.txt").expect("Could not open exceptions.txt");
+    let file = File::open("./exceptions.txt").expect("Could not open exceptions.txt");
     let reader = BufReader::new(file);
     let mut exceptions = Vec::new();
 
@@ -59,38 +58,13 @@ fn get_exceptions() -> Vec<String> {
     exceptions
 }
 
-// i want to check if a length of the string is an exception
-// e.g. "without a worry in the world" -> "without a worry" is
-// so i need .iter() or something to look for that
 fn check_exceptions(string: &str) -> bool {
     let exceptions = get_exceptions();
     exceptions.contains(&string.to_string())
 }
 
 fn main() {
-    let input = "without a worry in the world";
+    let input = "without a worry";
     let output = to_title_case(input);
     println!("{}", output);
-}
-
-
-// i have no clue why these tests dont work
-// the functions work fine in main
-// but it panics when i run the tests
-// i guess i didnt study enough in rustlings...
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn check_title_case() {
-        assert_eq!(to_title_case("hello world"), "Hello World");
-        assert_eq!(to_title_case("what a wonderful world"), "What a Wonderful World");
-        assert_eq!(to_title_case("without a worry in the world"), "without a worry in the World");
-    }
-
-    #[test]
-    fn check_exceptions_file() {
-        assert!(check_exceptions("a"));
-        assert!(check_exceptions("as"));
-    }
 }
