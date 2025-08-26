d9a81942cf0636fddb70e71034a8aefa19f2b4e2
numberss <red.ace.l@outlook.com>
2025-08-26 23:00:06 +0100

    getting started

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,78 @@
+// this is a program to convert a string to title case
+// e.g. "hello world" -> "Hello World"
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn to_title_case(string: &str) -> String {
+    let mut result = String::new();
+    let split_string = string.split(' ');
+
+    if check_exceptions(string) {
+        return string.to_string();
+    }
+
+    for word in split_string {
+        // check to see if its an exception
+        if check_exceptions(word) {
+            result.push(word.chars().next().unwrap());
+            result.push(' ');
+            continue;
+        };
+
+        let mut chars = word.chars();
+
+        if let Some(first_char) = chars.next() {
+            result.push(first_char.to_ascii_uppercase());
+            for c in chars {
+                result.push(c.to_ascii_lowercase());
+            }
+            result.push(' ');
+        }
+    }
+    result.trim().to_string()
+}
+
+fn get_exceptions() -> Vec<String> {
+    let file = File::open("exceptions.txt").expect("Could not open exceptions.txt");
+    let reader = BufReader::new(file);
+    let mut exceptions = Vec::new();
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        // skip comments and empty lines
+        if line.starts_with('#') || line.trim().is_empty() {
+            continue;
+        }
+        // remove whitespace
+        let trimmed = line.trim();
+
+        if !trimmed.is_empty() && !trimmed.starts_with('#') {
+            exceptions.push(trimmed.to_string());
+        }
+    }
+    exceptions
+}
+
+fn check_exceptions(string: &str) -> bool {
+    let exceptions = get_exceptions();
+    exceptions.contains(&string.to_string())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = "hello  world from   rust";
+    let output = to_title_case(input);
+    println!("{}", output);
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn check_title_case() {
+        assert_eq!(to_title_case("hello world"), "Hello World");
+        assert_eq!(to_title_case("what a wonderful world"), "What a Wonderful World");
+        assert_eq!(to_title_case("without a worry in the world"), "without a worry in the World");
+    }
 }
